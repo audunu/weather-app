a9f861015ba7aaca6daa9dfa880e9d91a6e85b93
audunu <audunu@gmail.com>
2022-03-18 09:01:05 +0100

    basic api functionality working

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
 const searchField = document.querySelector('#search');
 const submitButton = document.querySelector('input[type=submit]');
-const mainDiv = document.querySelector('.main');
-
+const skyDiv = document.querySelector('.sky');
+const tempDiv = document.querySelector('.temp');
+const windDiv = document.querySelector('.wind');
 
 
 async function getCityForecast() {
-    const searchTerm = searchField.value;
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&APPID=280ce5c26594bc7d59eb30e9d8d85641`, { mode: 'cors' })
-    const weatherData = await response.json();
-    return {
-        description: weatherData.weather[0].description,
-        temperature: weatherData.main.temp,
-        wind: weatherData.wind.speed,    
+    try {
+        const searchTerm = searchField.value;
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&APPID=280ce5c26594bc7d59eb30e9d8d85641`, { mode: 'cors' })
+        const weatherData = await response.json();
+        return {
+            description: weatherData.weather[0].description,
+            temperature: weatherData.main.temp,
+            wind: weatherData.wind.speed
+        }
+    } catch (error) {
+        alert(error);
     }
 
-    };
+};
 
-getCityForecast();
 
-/* submitButton.addEventListener('click', (e) => {
+submitButton.addEventListener('click', (e) => {
     e.preventDefault();
     getCityForecast();
+    displayWeather();
 });
- */
 
-function displayWeather() {
-    mainDiv.innerHTML = getCityForecast().description;
+async function displayWeather() {
+    try {
+        skyDiv.innerHTML = (await getCityForecast()).description;
+        tempDiv.innerHTML = (await getCityForecast()).temperature;
+        windDiv.innerHTML = (await getCityForecast()).wind;
+    } catch (error) {
+        alert(error);
+    }
 }
-
-displayWeather();
